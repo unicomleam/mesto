fc9e0c9aa4a86ebea13288a6c539709ab1a4bf18
Alexandr Prudnikov <prudnikov20@yandex.ru>
2023-01-03 04:30:14 +0300

    add fonts

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popupClose = document.querySelector('.popup__close')
+const popupClose = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 const popupSubmit = document.querySelector('.popup__submit');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
+let popupName = document.querySelector('.popup__input_type_name');
+let popupProfession = document.querySelector('.popup__input_type_profession');
+
+popupName.value = profileName.textContent;
+popupProfession.value = profileProfession.textContent;
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -14,13 +20,16 @@ function closePopup() {
 }
 
 function redactInfo() {
-    let name = document.querySelector('.popup__input_type_name');
-    let profession = document.querySelector('.popup__input_type_profession');
-
-    profileName.textContent = name.value;
-    profileProfession.textContent = profession.value;
+    profileName.textContent = popupName.value;
+    profileProfession.textContent = popupProfession.value;
+    closePopup()
 }
 
 editButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 popupSubmit.addEventListener('click', redactInfo);
+popup.addEventListener('click', function(event) {
+    if (event.target === event.currentTarget) {
+        closePopup();
+    }
+});
