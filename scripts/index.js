ca13cd939e56aaa0fd5b66e02e24aba70ab53c50
Alexandr Prudnikov <prudnikov20@yandex.ru>
2023-01-05 08:16:01 +0300

    finale fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popupClose = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-const popupSubmit = document.querySelector('.popup__submit');
+const popupEditForm = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 let popupName = document.querySelector('.popup__input_type_name');
 let popupProfession = document.querySelector('.popup__input_type_profession');
 
-popupName.value = profileName.textContent;
-popupProfession.value = profileProfession.textContent;
-
 
 function openPopup() {
+    popupName.value = profileName.textContent;
+    popupProfession.value = profileProfession.textContent;
     popup.classList.add('popup_opened');
 }
 
@@ -19,7 +18,8 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-function redactInfo() {
+function redactInfo(evt) {
+    evt.preventDefault();
     profileName.textContent = popupName.value;
     profileProfession.textContent = popupProfession.value;
     closePopup()
@@ -27,7 +27,7 @@ function redactInfo() {
 
 editButton.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
-popupSubmit.addEventListener('click', redactInfo);
+popupEditForm.addEventListener('submit', redactInfo);
 popup.addEventListener('click', function(event) {
     if (event.target === event.currentTarget) {
         closePopup();
