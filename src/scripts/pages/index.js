71017fd374f179d94048f794b7b01a6905ea2644
Alexandr Prudnikov <prudnikov20@yandex.ru>
2023-04-22 18:05:50 +0300

    Global update

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -4,6 +4,7 @@ import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
+import '../../page/index.css';
 
 import {
     nameConfig,
